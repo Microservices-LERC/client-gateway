db83fa790bd604b12ffe714af7a2382d620b0ab0
luis <l.ramirez@intrare.mx>
2024-07-04 14:28:41 -0600

    Health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { NatsModule } from './nats/nats.module';
+import { HealthCheckModule } from './health-check/health-check.module';
 
 @Module({
-  imports: [ProductsModule, OrdersModule, NatsModule],
+  imports: [ProductsModule, OrdersModule, NatsModule, HealthCheckModule],
 })
 export class AppModule {}
